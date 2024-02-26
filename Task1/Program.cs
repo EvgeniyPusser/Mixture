a8adf7d1f681d467f1c7891c065d6b5197332b61
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-26 18:51:48 +0300

    New struggle

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿void Main(){
+
+Recursive();
+
+}
+
+void Recursive(){
+    int n  = 1;
+    if(n == 9){
+        return}
+   Console.Write($"In my pocket now is only{n} bucks");
+    n++;
+    Recursive();}
+     
+
+
+ Main();
